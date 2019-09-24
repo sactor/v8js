a5c7899fe3ec8de9126c21a6980f9e5bb12a2e4d
Antti Kumpulainen <sac@crasman.fi>
2019-09-24 10:24:21 +0300

    Fix timezone problem still occurring before first timezone change Fix time zone changed notifying for V8 v.7.5 and above

--- v8js_v8.cc
+++ v8js_v8.cc
@@ -130,10 +130,7 @@ void v8js_v8_call(v8js_ctx *c, zval **return_value,
 	tz = getenv("TZ");
 
 	if (tz != NULL) {
-		if (c->tz == NULL) {
-			c->tz = strdup(tz);
-		}
-		else if (strcmp(c->tz, tz) != 0) {
+		if (c->tz == NULL || strcmp(c->tz, tz) != 0) {
 #if (V8_MAJOR_VERSION < 7 || (V8_MAJOR_VERSION == 7 && V8_MINOR_VERSION < 5)) && !V8JS_V8_TIME_ZONE_REDETECTION_SUPPORTED
 			v8::Date::DateTimeConfigurationChangeNotification(c->isolate);
 #else
